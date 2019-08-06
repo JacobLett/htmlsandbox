b44cd9cfd74e90926d93350791eb3a19911cb699
Jacob Lett <jlett@altair.com>
2019-08-06 16:08:05 -0400

    made modal wider

--- altair-sales-win/scripts.js
+++ altair-sales-win/scripts.js
@@ -53,7 +53,10 @@
     var salesList = new List('sales-list', options);
 
 
-
+    $(".modal-wide").on("show.bs.modal", function() {
+        var height = $(window).height() - 200;
+        $(this).find(".modal-body").css("max-height", height);
+      });
      
    // document ready  
    });
